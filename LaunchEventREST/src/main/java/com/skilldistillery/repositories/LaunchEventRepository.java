870707d92ec4bba207d471c71773ebddd4d787f7
Andy Cary <info@andycary.com>
2022-05-06 19:11:38 -0400

    search for event title like - add param in repository

--- LaunchEventREST/src/main/java/com/skilldistillery/repositories/LaunchEventRepository.java
+++ LaunchEventREST/src/main/java/com/skilldistillery/repositories/LaunchEventRepository.java
@@ -3,11 +3,12 @@ package com.skilldistillery.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.entities.LaunchEvent;
 
 public interface LaunchEventRepository extends JpaRepository<LaunchEvent, Integer> {
 
-	List<LaunchEvent> findByTitleLike(String keyword);
+	List<LaunchEvent> findByTitleLike(@Param("keyword") String keyword);
 
 }
